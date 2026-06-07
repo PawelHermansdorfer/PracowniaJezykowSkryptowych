2fff4040d451ac1c69d954a60b0c15a4b51219fb
Pawel Hermansdorfer <pawelhermansdorfer@gmail.com>
2026-06-07 19:14:20 +0200

    Update

--- JS/main.js
+++ JS/main.js
@@ -20,21 +20,26 @@ const config = {
 new Phaser.Game(config);
 
 
-let level_layout = "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 \
-                    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 \
-                    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 \
-                    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 \
-                    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 \
-                    0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 \
-                    0 0 0 0 0 0 0 1 1 1 0 0 0 0 0 0 \
-                    X 0 0 0 0 0 0 0 0 0 0 0 0 0 0 M \
-                    1 1 1 1 1 1 0 0 0 0 0 1 1 1 1 1 \
-"
+let level_layout = ` 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                     0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                     0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                     0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                     0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                     0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                     0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 M
+                     0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 1
+                     0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0
+                     0 0 0 0 0 0 1 1 1 0 0 0 1 0 0 0 1 1 0 0 0
+                     X 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0
+                     1 1 1 1 1 0 0 0 0 0 1 1 1 1 1 0 0 0 0 0 0`;
 
 
 let player;
 let cursors;
 let platforms;
+
+let jump_vel;
+let move_vel;
 
 let doors;
 
@@ -43,7 +48,7 @@ function
 createRect(scene, x, y, w, h, color, group)
 {
     const rect = scene.add.rectangle(x, y, w, h, color);
-    rect.setOrigin(0, 0);
+    rect.setOrigin(0.5, 0.5);
     scene.physics.add.existing(rect, true);
     rect.body.setSize(w, h);
     rect.body.updateFromGameObject();
@@ -59,26 +64,85 @@ finish_level()
 
 
 function
-load_level(layout)
+load_level(scene, layout)
 {
+    let w = scene.scale.width;
+    let h = scene.scale.height;
+
+    // Find size of level
     let dim_x = 0;
+    let current_dim_x = 0;
     let dim_y = 0;
-for (const ch of level_layout)
+    for(const ch of layout)
     {
         if (ch === '0' || ch === '1' || ch === 'X' || ch === 'M')
         {
-            currentRow.push(ch);
+            current_dim_x += 1;
         }
-        else if (ch === '\\')
+        else if (ch === '\n')
         {
-            if (currentRow.length > 0)
-            {
-                rows.push(currentRow);
-                currentRow = [];
-            }
+            dim_x = Math.max(dim_x, current_dim_x);
+            current_dim_x = 0;
+            dim_y += 1;
         }
     }
+    let tile_dim_x = (w/(dim_x));
+    let tile_dim_y = (h/(dim_y+1));
+
+    // Load level
+    let result_player;
+    let result_platforms = scene.physics.add.staticGroup();
+    let result_doors     = scene.physics.add.staticGroup();
+    let result_jump_vel = Math.sqrt(2*scene.physics.world.gravity.y *  2*tile_dim_y * 1.1);
+    let result_move_vel = tile_dim_x * 3;
+    let pos_x = 0;
+    let pos_y = 0;
+    for(const ch of layout)
+    {
+        let tile_center_x = pos_x*tile_dim_x + tile_dim_x/2;
+        let tile_center_y = pos_y*tile_dim_y + tile_dim_y/2;
+
+        if(ch === '1')
+        {
+            createRect(scene, tile_center_x, tile_center_y, tile_dim_x, tile_dim_y, 0x00ff00, result_platforms);
+            pos_x += 1;
+        }
+        else if(ch === 'X')
+        {
+            result_player = scene.add.rectangle(tile_center_x, tile_center_y, 20, 40, 0x0000ff);
+            scene.physics.add.existing(result_player);
+            result_player.body.setDragX(5000);
+            pos_x += 1;
+        }
+        else if(ch === 'M')
+        {
+            createRect(scene, tile_center_x, tile_center_y, tile_dim_x, tile_dim_y, 0x020202, result_doors);
+            pos_x += 1;
+        }
+        else if(ch === '0')
+        {
+            pos_x += 1;
+        }
+        else if (ch === '\n')
+        {
+            pos_x = 0;
+            pos_y += 1;
+        }
+    }
+
+    // Pack and send
+    scene.physics.add.collider(result_player, result_platforms);
+    scene.physics.add.overlap(result_player, result_doors, finish_level, 0, scene);
+    let result = [
+        result_player,
+        result_platforms,
+        result_doors,
+        result_jump_vel,
+        result_move_vel,
+    ];
+    return result;
 }
+
 
 
 ////////////////////////////////////////
@@ -90,24 +154,12 @@ create()
 
     cursors = this.input.keyboard.createCursorKeys();
 
-    // Player
-    player = this.add.rectangle(100, 450, 20, 40, 0x0000ff);
-    this.physics.add.existing(player);
-    player.body.setCollideWorldBounds(true);
-    player.body.setDragX(5000);
-
-    // Ground
-    platforms = this.physics.add.staticGroup();
-    createRect(this, 0, h - 100, 500, 50, 0x00ff00, platforms);
-    createRect(this, 700, h - 100, 500, 50, 0x00ff00, platforms);
-
-    this.physics.add.collider(player, platforms);
-
-    // Target
-    doors = this.physics.add.staticGroup();
-    createRect(this, w-50, h-100-100, 50, 100, 0x020202, doors)
-
-    this.physics.add.overlap(player, doors, finish_level, 0, this);
+    let level_data = load_level(this, level_layout);
+    player = level_data[0];
+    platforms = level_data[1];
+    doors = level_data[2];
+    jump_vel = level_data[3];
+    move_vel = level_data[4];
 }
 
 
@@ -115,6 +167,9 @@ create()
 function
 update()
 {
+    let w = this.scale.width;
+    let h = this.scale.height;
+
     let x_dir = 0;
     if(cursors.right.isDown)
     {
@@ -124,24 +179,16 @@ update()
     {
         x_dir -= 1;
     }
+    player.body.setVelocityX(x_dir * move_vel);
 
-    if(x_dir != 0)
+
+    if (cursors.up.isDown && player.body.touching.down)
     {
-        player.body.setVelocityX(x_dir * 100);
+        player.body.setVelocityY(-jump_vel);
     }
-    player.body.setAccelerationX(x_dir * 10000);
 
 
-
-    let y_dir = 0
-    if(cursors.up.isDown && player.body.touching.down)
-    {
-        y_dir -= 1
-    }
-    player.body.setAccelerationY(y_dir * 30000);
-
-
-    if(player.y > 650)
+    if(player.y > h + 200)
     {
         this.scene.restart();
     }
