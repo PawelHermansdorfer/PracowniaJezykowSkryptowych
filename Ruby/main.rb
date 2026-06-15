0d14cf02ad7d5a2506158d3d2d4499311947d80c
Pawel Hermansdorfer <pawelhermansdorfer@gmail.com>
2026-06-15 02:22:03 +0200

    Update

--- Ruby/main.rb
+++ Ruby/main.rb
@@ -1,18 +1,36 @@
-require 'nokogiri'
-require 'httparty'
+require "httparty"
+require "nokogiri"
 
-url = 'http://books.toscrape.com/'
+def list_amazon_items(query)
+  url = "https://www.amazon.com/s?k=#{query}"
+  response = HTTParty.get(url, headers: { "User-Agent" => "Mozilla/5.0" })
+  doc = Nokogiri::HTML(response.body)
 
-response = HTTParty.get(url)
-document = Nokogiri::HTML(response.body)
+  items = []
+  doc.css("div.s-result-item[data-asin]").each do |item|
+    asin = item["data-asin"]
+    next if asin.nil? || asin.empty?
 
-books = document.css('.product_pod')
+    title_node = item.at("h2 span")
+    next unless title_node
 
-books.each do |book|
-  title = book.css('h3 a').attribute('title').value
-  price = book.css('.price_color').text
-  
-  puts "Title: #{title}"
-  puts "price:  #{price}"
+    title = title_node.text.strip
+    price_span = item.at("span.a-price span.a-offscreen")
+    price = price_span ? price_span.text.strip : nil
+
+    items << {
+      title: title,
+      price: price || 'nil'
+    }
+  end
+
+  items
+end
+
+items = list_amazon_items("pokemon")
+
+items.each_with_index do |item|
+  puts "#{item[:title]}"
+  puts "#{item[:price]}"
   puts "\n"
 end
