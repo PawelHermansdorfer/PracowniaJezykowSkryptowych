257d5bde24c650fb1a59c7585a9dfbbe95ca51be
Pawel Hermansdorfer <pawelhermansdorfer@gmail.com>
2026-06-15 20:17:54 +0200

    Update

--- Ruby/main.rb
+++ Ruby/main.rb
@@ -6,9 +6,11 @@ driver = Selenium::WebDriver.for :chrome
 driver.navigate.to "https://www.amazon.com"
 sleep 1
 
+print "Enter search phrase: "
+query = gets.chomp
 
 search_box = wait.until { driver.find_element(name: 'field-keywords') }
-search_box.send_keys("pokemon")
+search_box.send_keys(query)
 
 btn = driver.find_element(id: 'nav-search-submit-button')
 btn.click
@@ -21,6 +23,9 @@ items.first(20).each do |item|
   price_whole      = item.find_elements(css: '.a-price-whole').first&.text
   price_fractional = item.find_elements(css: '.a-price-fraction').first&.text
   currancy         = item.find_elements(css: '.a-price-symbol').first&.text
+  link = item.find_elements(css: 'a.a-link-normal.s-no-outline').first
+  link = link&.attribute('href')
+
   puts title
   puts "#{price_whole}.#{price_fractional}#{currancy}"
   puts "\n"
