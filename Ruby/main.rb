5ad22180daf61c05cde4ff14353fe621fd945362
Pawel Hermansdorfer <pawelhermansdorfer@gmail.com>
2026-06-15 20:58:30 +0200

    Update

--- Ruby/main.rb
+++ Ruby/main.rb
@@ -3,11 +3,12 @@ require 'selenium-webdriver'
 wait   = Selenium::WebDriver::Wait.new(timeout: 15)
 driver = Selenium::WebDriver.for :chrome
 
-driver.navigate.to "https://www.amazon.com"
+driver.navigate.to "https://www.amazon.pl"
 sleep 1
 
 print "Enter search phrase: "
-query = gets.chomp
+# query = gets.chomp
+query = "pokemon"
 
 search_box = wait.until { driver.find_element(name: 'field-keywords') }
 search_box.send_keys(query)
@@ -18,15 +19,47 @@ btn.click
 wait.until { driver.find_elements(css: 'div.s-main-slot div[data-component-type="s-search-result"]').any? }
 items = driver.find_elements(css: 'div.s-main-slot div[data-component-type="s-search-result"]')
 
-items.first(20).each do |item|
-  title            = item.find_elements(css: 'h2 span').first&.text
+results = []
+
+items.first(5).each do |item|
+  title = item.find_elements(css: 'h2 span').first&.text
+
   price_whole      = item.find_elements(css: '.a-price-whole').first&.text
   price_fractional = item.find_elements(css: '.a-price-fraction').first&.text
-  currancy         = item.find_elements(css: '.a-price-symbol').first&.text
-  # link = item.find_elements(css: 'a.a-link-normal.s-no-outline').first
-  # link = link&.attribute('href')
+  currency         = item.find_elements(css: '.a-price-symbol').first&.text
 
-  puts title
-  puts "#{price_whole}.#{price_fractional}#{currancy}"
+  link_el = item.find_elements(css: 'a.a-link-normal.s-no-outline').first
+  link    = link_el&.attribute('href')
+
+  results << {
+    title: title,
+    price: "#{price_whole}.#{price_fractional}#{currency}",
+    url: link
+  }
+end
+
+results.each do |item|
+  next unless item[:url]
+  driver.navigate.to(item[:url])
+
+  delivery = wait.until do
+    el = driver.find_elements(css: 'span[data-csa-c-content-id="DEXUnifiedCXPDM"]').first
+    el if el && el.displayed?
+  end
+  delivery_info = {
+    price: delivery&.attribute('data-csa-c-delivery-price'),
+    time: delivery&.attribute('data-csa-c-delivery-time'),
+    type: delivery&.attribute('data-csa-c-delivery-type'),
+    condition: delivery&.attribute('data-csa-c-delivery-condition'),
+    text: delivery&.text&.strip
+  }
+
+  item[:delivery] = delivery_info
+end
+
+results.each do |item|
+  puts item[:title]
+  puts item[:price]
+  puts item[:delivery]
   puts "\n"
 end
